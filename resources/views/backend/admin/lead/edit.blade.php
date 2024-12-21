669181377db15fed53e63f63d3b0439403b6f890
db-hasan <infoalihasanbd@gmail.com>
2024-12-22 00:21:59 +0600

    update design

--- resources/views/backend/admin/lead/edit.blade.php
+++ resources/views/backend/admin/lead/edit.blade.php
@@ -1,65 +1,159 @@
 @extends('backend.layouts')
 @section('content')
+    <style>
+        .nav-tabs-bordered .nav-link.active {
+            background-color: #121010d9 !important;
+            color: #fff;
+            border-bottom: 4px solid #19c745;
+        }
+    </style>
     <main id="main" class="main">
         <div class="d-flex justify-content-between">
             <div class="pagetitle">
-                <h1>Lead List</h1>
+                <h1>Student Profile</h1>
                 <nav>
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
-                        <li class="breadcrumb-item active">View</li>
+                        <li class="breadcrumb-item"><a href="">Home</a></li>
+                        <li class="breadcrumb-item active">Edit</li>
                     </ol>
                 </nav>
             </div>
-            <div class="text-end pt-2">
-                <a href="{{ route('lead.create') }}" class="btn btn-primary"><i class="fas fa-plus-circle"></i>
-                    Add lead</a>
-            </div>
         </div>
         <hr>
-        <div class="custom-scrollbar-table">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Marketing</th>
-                        <th>Resource</th>
-                        <th>Name</th>
-                        <th>Number</th>
-                        <th>Email</th>
-                        <th>Addrss</th>
-                        <th>Zip</th>
-                        <th class="text-end">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>01</td>
-                        <td>Rony</td>
-                        <td>Linkedin</td>
-                        <td>Mim</td>
-                        <td>01723629080</td>
-                        <td>info@gmail.com</td>
-                        <td>shibonj-bogura</td>
-                        <td>5810</td>
-                        <td class="d-flex justify-content-end">
-                            <a href="" class="btn btn-primary mx-1">
-                                <i class="bi bi-eye"></i>
-                            </a>
-                            <a href="" class="btn btn-primary mx-1">
-                                <i class="bi bi-pencil-square"></i>
-                            </a>
-                            <form class="deleteForm" action="" method="POST">
+        <div class="col-xl-12">
+
+            <div class="card">
+                <div class="card-body pt-3">
+                    <!-- Bordered Tabs -->
+                    {{-- <ul class="nav nav-tabs nav-tabs-bordered"> --}}
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item ">
+                            <button class="nav-link px-4 card-title active" data-bs-toggle="tab"
+                                data-bs-target="#personal">Personal
+                                <br>
+                                Information</button>
+                        </li>
+                        <li class="nav-item">
+                            <button class="nav-link px-4 card-title" data-bs-toggle="tab"
+                                data-bs-target="#education">Education <br>
+                                Training</button>
+                        </li>
+                        <li class="nav-item">
+                            <button class="nav-link px-4 card-title" data-bs-toggle="tab"
+                                data-bs-target="#experience">Working <br>
+                                Experience</button>
+                        </li>
+                        <li class="nav-item">
+                            <button class="nav-link px-4 card-title" data-bs-toggle="tab"
+                                data-bs-target="#application">Addmission <br>
+                                Application</button>
+                        </li>
+                        <li class="nav-item">
+                            <button class="nav-link px-4 card-title" data-bs-toggle="tab"
+                                data-bs-target="#security-deposit">Security
+                                <br>
+                                Deposit</button>
+                        </li>
+
+                        <li class="nav-item">
+                            <button class="nav-link px-4 card-title" data-bs-toggle="tab"
+                                data-bs-target="#change-password">Change <br>
+                                Password</button>
+                        </li>
+                    </ul>
+
+                    <div class="tab-content pt-2">
+
+                        <div class="tab-pane fade show active" id="personal">
+                            <h5 class="card-title">Address personal</h5>
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 label">Address personal</div>
+                                <div class="col-lg-9 col-md-8">Dhaka Bangladesh</div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade pt-3" id="education">
+                            <h5 class="card-title"></h5>
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 label">Address education</div>
+                                <div class="col-lg-9 col-md-8">Dhaka Bangladesh</div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade pt-3" id="experience">
+                            <h5 class="card-title"></h5>
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 label">Address experience</div>
+                                <div class="col-lg-9 col-md-8">Dhaka Bangladesh</div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade pt-3" id="application">
+                            <h5 class="card-title"></h5>
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 label">Address experience</div>
+                                <div class="col-lg-9 col-md-8">Dhaka Bangladesh</div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade pt-3" id="security-deposit">
+                            <h5 class="card-title"></h5>
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 label">security-deposit</div>
+                                <div class="col-lg-9 col-md-8">Dhaka Bangladesh</div>
+                            </div>
+                        </div>
+
+
+                        <div class="tab-pane fade pt-3" id="change-password">
+                            <!-- Change Password Form -->
+                            <form method="post" action="" enctype="multipart/form-data" class="row g-3 p-3">
                                 @csrf
-                                @method('DELETE')
-                                <button type="button" class="btn btn-danger btnDelete"><i
-                                        class="bi bi-trash"></i></button>
-                            </form>
-                            
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+
+                                <div class="row mb-3">
+                                    <label for="old_password" class="col-md-4 col-lg-3 col-form-label">Current
+                                        Password<span class="text-danger">*</span></label>
+                                    <div class="col-md-8 col-lg-9">
+                                        <input type="password" class="form-control" id="old_password" name="old_password"
+                                            value="">
+                                        @error('old_password')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="new_password" class="col-md-4 col-lg-3 col-form-label">New
+                                        Password<span class="text-danger">*</span></label>
+                                    <div class="col-md-8 col-lg-9">
+                                        <input type="password" class="form-control" id="new_password" name="new_password"
+                                            value="">
+                                        @error('new_password')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <label for="new_password_confirmation" class="col-md-4 col-lg-3 col-form-label">Confirm
+                                        Password<span class="text-danger">*</span></label>
+                                    <div class="col-md-8 col-lg-9">
+                                        <input type="password" class="form-control" id="new_password_confirmation"
+                                            name="new_password_confirmation" value="">
+                                        @error('new_password_confirmation')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-primary">Change Password</button>
+                                </div>
+                            </form><!-- End Change Password Form -->
+
+                        </div>
+
+                    </div><!-- End Bordered Tabs -->
+
+                </div>
+            </div>
+
         </div>
     </main>
     <script src="{{ asset('backend/js/jquery-3.7.1.min.js') }} "></script>
--- resources/views/backend/header.blade.php
+++ resources/views/backend/header.blade.php
@@ -20,6 +20,7 @@
     <!-- Vendor CSS Files -->
     <link href="{{ asset('backend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('backend/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
